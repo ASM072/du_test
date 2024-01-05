d7e19f917922c97980ce4aa449d2adef355c0c58
ASM072 <ahmedsaadmalik8@gmail.com>
2024-01-05 17:18:42 +0400

    First Commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,18 @@
-import logo from './logo.svg';
 import './App.css';
+import PlanCard from './PlanCard';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      
+      <div style={ {
+        display: 'flex',
+        // justifyContent: 'center',
+        // alignItems: 'center',
+        height: '900px'
+      }}>
+        <PlanCard/>
+      </div>
     </div>
   );
 }
---
+++ src/PlanCardCSS.js
@@ -0,0 +1,73 @@
+// color1 = #fc00ff
+// color2 = #00dbde
+import React from "react";
+import styled, { css } from "styled-components";
+
+// const gradient = degs => css`
+    
+//     // background: linear-gradient(
+//     //     ${ degs || 130 }deg,
+//     //     #fc00ff 100%,
+//     //     #00dbd3 0%
+//     // );
+// `;
+
+export const Card = styled.div`
+    position: absolute;
+    overflow: hidden;
+    border-radius: 25px;
+    border-left: 4px solid red;
+    width: 400px;
+    margin: 3rem 3rem 3rem 3rem;
+    padding: 3rem 0 2rem;
+    border-radius: 0.9rem;
+    color: white;
+    background: linear-gradient( #00dbd3 0%, #fc00ff 100%);
+    box-shadow: 
+        0 24px 38px 3px rgba(0,0,0,0,025),
+        0 9px 46px 8px rgba(0,0,0,0.025),
+        0 11px 15px -7px rgba(0,0,0,0,025);
+
+`;
+
+export const Content = styled.div`
+    position: relative;
+    z-index: 3;
+    display: flex;
+    margin-left: 1rem;
+    padding-left: 0.5rem;
+    flex-direction:column;
+    align-items: flex-start;
+    justify-content: center;
+`
+
+export const FirstDetail = styled.div`
+    color: #00dbde;
+    font-size: 1.25rem;
+    border-bottom: 1px solid #bbb;
+`
+export const SecondDetail = styled.div`
+    color: #00dbde;
+    font-size: 1.25rem;
+    border-bottom: 1px solid #bbb;
+
+`
+export const ThirdDetail = styled.div`
+    color: #00dbde;
+    font-size: 1.25rem;
+    border-bottom: 1px solid #bbb;
+
+`
+export const ForthDetail = styled.div`
+    color: #00dbde;
+    font-size: 1.25rem;
+    border-bottom: 1px solid #bbb;
+`
+export const SelectButton = styled.div`
+    align-items: flex-end;
+    height: 40px;
+    padding: 0 2rem;
+    border: 0;
+    border-radius: 10px;
+    background: white;
+`
